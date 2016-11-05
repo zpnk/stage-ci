f8590f59943d15ca19b3c619aa561e86860bb5c0
Andrew McCloud <andrew@amccloud.com>
2016-11-05 03:33:59 -0700

    Use built-in now

--- core.js
+++ core.js
@@ -1,12 +1,15 @@
+const path = require('path');
 const {exec} = require('child_process');
+
+const NOW = path.resolve('./node_modules/now/build/bin/now');
 
 function stage(cwd, {alias}) {
   return new Promise((resolve) => {
-    const nowProc = exec(`now`, {cwd});
+    const nowProc = exec(NOW, {cwd});
     nowProc.stdout.on('data', (url) => {
       if (!url) return;
       console.log(`> Aliasing ${url}`);
-      const aliasProc = exec(`now alias set ${url} ${alias}`, {cwd});
+      const aliasProc = exec(`${NOW} alias set ${url} ${alias}`, {cwd});
       aliasProc.on('close', (code) => {
         console.log(`> Ready! ${alias}`);
         resolve(alias);
