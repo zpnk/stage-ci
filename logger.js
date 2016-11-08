89726b322820f8def3af5446b871eded739a47c0
Andrew McCloud <andrew@amccloud.com>
2016-11-08 15:11:52 -0800

    Guard require

--- logger.js
+++ logger.js
@@ -1,9 +1,11 @@
 const winston = require('winston');
-require('winston-papertrail').Papertrail;
 const {name} = require('./package.json');
+
 const {PAPERTRAIL_HOST, PAPERTRAIL_PORT} = process.env;
 
 if (PAPERTRAIL_HOST && PAPERTRAIL_PORT) {
+  require('winston-papertrail').Papertrail;
+
   const winstonPapertrail = new winston.transports.Papertrail({
     host: PAPERTRAIL_HOST,
     port: PAPERTRAIL_PORT,
