8431396df057bc1868dd007d616ce7a13a04062b
zpnk <z@zpnk.me>
2016-11-08 14:52:02 -0800

    Add app name as papertrail hostname.

--- logger.js
+++ logger.js
@@ -1,11 +1,13 @@
 const winston = require('winston');
 require('winston-papertrail').Papertrail;
+const {name} = require('./package.json');
 const {PAPERTRAIL_HOST, PAPERTRAIL_PORT} = process.env;
 
 if (PAPERTRAIL_HOST && PAPERTRAIL_PORT) {
   const winstonPapertrail = new winston.transports.Papertrail({
     host: PAPERTRAIL_HOST,
     port: PAPERTRAIL_PORT,
+    hostname: name,
     handleExceptions: true,
     json: true
   });
