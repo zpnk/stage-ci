693f8aaa59be907f5fb91b0ad81bb20abc6d5914
Thomas Sattlecker <thomas.sattlecker@gmail.com>
2017-11-17 08:14:46 +0100

    supporting longer branch names

--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,14 @@
 const {format, parse} = require('url');
 
 const INVALID_URI_CHARACTERS = /\//g;
+const MAXIMUM_URI_CHARACTER_COUNT = 62;
 
 exports.createAliasUrl = (repo, ref) => {
   const repoStripped = repo.replace(/[^A-Z0-9]/ig, '-');
   const refStripped = ref.replace(INVALID_URI_CHARACTERS, '-');
-  return `https://${repoStripped}-${refStripped}.now.sh`;
+  const aliasUrl = `${repoStripped}-${refStripped}`;
+  const aliasUrlStripped = aliasUrl.substring(0, MAXIMUM_URI_CHARACTER_COUNT).replace(/-$/, '');
+  return `https://${aliasUrlStripped}.now.sh`;
 };
 
 exports.createCloneUrl = (cloneUrl, token) => {
